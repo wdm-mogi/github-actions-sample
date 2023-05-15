988d7adef5ca3c87f0f6e2a53d355077e8e7da4e
mogi <mogi@wdm.co.jp>
2023-05-15 09:58:52 +0900

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(Program);
+
+            Console.WriteLine(type.FullName);
+
+            Console.ReadLine();
+        }
+    }
+}
